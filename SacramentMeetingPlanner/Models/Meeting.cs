030b5d9f586927df7cd30f1ab89b61374eac1a7f
Irina Ohara <irina.v.ohara@gmail.com>
2019-04-07 15:31:53 -0600

    MVC, DB, migrations, search

---
+++ SacramentMeetingPlanner/Models/Meeting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        [Display(Name = "Meeting Date")]
+        [DataType(DataType.Date)]
+        public DateTime MeetingDate { get; set; }
+        public string Conducting { get; set; }
+        public string Presiding { get; set; }
+        //prayers
+        public string Invocation { get; set; }
+        public string Benediction { get; set; }
+        //songs
+        [Display(Name = "Opening Hymn")]
+        public int OpeningHymn { get; set; }
+        [Display(Name = "Sacrament Hymn")]
+        public int SacramentHymn { get; set; }
+        [Display(Name = "Closing Hymn")]
+        public int ClosingHymn { get; set; }
+    }
+}
---
+++ SacramentMeetingPlanner/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class SeedData
+    {
+    }
+} */
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SacramentMeetingPlanner.Models;
+using System;
+using System.Linq;
+
+namespace SacramentMeeting.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new SacramentMeetingPlannerContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<SacramentMeetingPlannerContext>>()))
+            {
+                // Look for any meetings.
+                if (context.Meeting.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Meeting.AddRange(
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-3-17"),
+                        Conducting = "Bruce Willies",
+                        Presiding = "Bruce Willies",
+                        Invocation = "Mary Jo",
+                        Benediction = "Bruce Lee",
+                        OpeningHymn = 344,
+                        SacramentHymn = 134,
+                        ClosingHymn = 235
+                    },
+
+                    new Meeting
+                    {
+                        MeetingDate = DateTime.Parse("2019-2-12"),
+                        Conducting = "Bruce Willies",
+                        Presiding = "Bruce Willies",
+                        Invocation = "Mary Jo",
+                        Benediction = "Bruce Lee",
+                        OpeningHymn = 344,
+                        SacramentHymn = 134,
+                        ClosingHymn = 235
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
+
