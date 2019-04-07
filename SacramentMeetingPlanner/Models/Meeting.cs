d8b5ecbba1120428704e1e323689e3aed7b41de9
Irina Ohara <irina.v.ohara@gmail.com>
2019-04-07 15:59:46 -0600

    Add validations

--- SacramentMeetingPlanner/Models/Meeting.cs
+++ SacramentMeetingPlanner/Models/Meeting.cs
@@ -12,16 +12,25 @@ namespace SacramentMeetingPlanner.Models
         [Display(Name = "Meeting Date")]
         [DataType(DataType.Date)]
         public DateTime MeetingDate { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Conducting { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Presiding { get; set; }
         //prayers
+        [StringLength(60, MinimumLength = 3)]
         public string Invocation { get; set; }
+        [StringLength(60, MinimumLength = 3)]
         public string Benediction { get; set; }
+        
         //songs
+        [Range(1, 341)]
         [Display(Name = "Opening Hymn")]
         public int OpeningHymn { get; set; }
+        [Range(1, 341)]
         [Display(Name = "Sacrament Hymn")]
         public int SacramentHymn { get; set; }
+        [Range(1, 341)]
         [Display(Name = "Closing Hymn")]
         public int ClosingHymn { get; set; }
     }
